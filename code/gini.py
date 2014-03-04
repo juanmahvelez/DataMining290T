522baf67ec21ed489be1cf5f4af5d086e46ccbf1
juanmahvelez <juanmahvelez@gmail.com>
2014-03-03 20:01:00 -0800

    Completed Homework gini

--- code/gini.py
+++ code/gini.py
@@ -5,6 +5,8 @@ Classes are strings."""
 
 import fileinput
 import csv
+import collections
+import itertools
 
 (
     CMTE_ID, AMNDT_IND, RPT_TP, TRANSACTION_PGI, IMAGE_NUM, TRANSACTION_TP,
@@ -20,9 +22,12 @@ CANDIDATES = {
 
 ############### Set up variables
 # TODO: declare datastructures
-
+x = collections.Counter()
+y = collections.Counter()
+z = collections.Counter()
 ############### Read through files
-for row in csv.reader(fileinput.input(), delimiter='|'):
+data = csv.reader(fileinput.input("../data/itpas2.txt"),delimiter='|')
+for row in data:
     candidate_id = row[CAND_ID]
     if candidate_id not in CANDIDATES:
         continue
@@ -32,11 +37,38 @@ for row in csv.reader(fileinput.input(), delimiter='|'):
     ###
     # TODO: save information to calculate Gini Index
     ##/
+    y.update([candidate_name])
+    z.update([zip_code])
+    if candidate_name == 'Romney':
+        x.update([zip_code])
+
+
+listZipGini = [] 
+for i in range(len(z.keys())):
+
+    if x.get(z.keys()[i]) is not None: 
+        zipRom = float(x.get(z.keys()[i]))
+
+    else:
+        zipRom = 0
+    zipObam = float(z.values()[i])-zipRom
+    zipTotal = float(z.values()[i])
+    zipClasses = [zipRom/zipTotal,zipObam/zipTotal]    
+    zipGini = float(1 - sum(frac**2 for frac in zipClasses))
+    listZipGini.append(zipGini)
+
+
+Rom = float(y.values()[0])
+Obam = float(y.values()[1])
+total = float(sum(y.values()))
+classes = [Rom/total,Obam/total]
+
+
 
 ###
 # TODO: calculate the values below:
-gini = 0  # current Gini Index using candidate name as the class
-split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
+gini = float(1 - sum(frac**2 for frac in classes))  # current Gini Index using candidate name as the class
+split_gini = float(sum(listZipGini)/len(listZipGini))  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
 
 print "Gini Index: %s" % gini
